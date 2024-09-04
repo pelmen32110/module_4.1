1608f2825ad27a321d84d19175766ebd681068e3
pelmen32110 <44937124+pelmen32110@users.noreply.github.com>
2024-09-05 03:15:51 +0700

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def devide(first,second):
+    if second == 0 :
+        print('Ошибка')
+    else:
+        result = first / second
+        print('Результат деления:', result)
+        return result
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+def devide(first,second):
+    if second == 0 :
+        print('Результат деления:', inf)
+    else:
+        result = first / second
+        print('Результат деления:', result )
+        return result
+
+
